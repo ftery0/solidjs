1ff5301bd9576a8c1ffc5dd814caf0987c45d80f
ftery0 <lerb5253@dgsw.hs.kr>
2025-06-30 18:28:14 +0900

    count

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Router, Route,  A } from "@solidjs/router";
+import { Router, Route, A } from "@solidjs/router";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Study from "./study";
 import Nav from "./ui/nav";
 
 
 export default function App() {
   return (
-    <Router>
-    <Route path="/" component={Home} />
-    <Route path="/profile" component={Profile} />
-  </Router>
+    <>
+      <Nav />
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/study" component={Study} />
+      </Router>
+    </>
   );
 }
